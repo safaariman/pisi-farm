e4b6c72501926b05e549d004034bd8b73c57a327
Safa Arıman <safa.ariman@sekomyazilim.com.tr>
2019-02-16 01:58:02 +0300

    updated admin page title

--- farm/admin/__init__.py
+++ farm/admin/__init__.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """ Created by Safa Arıman on 2/15/19 """
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from farm.admin.repository import RepositoryAdmin
 from farm.admin.package import PackageAdmin
 from farm.admin.job import JobAdmin
 
 __author__ = 'safaariman'
+
+
+admin.site.site_header = _('Pisi Farm Administration Console')
+admin.site.site_title = _('Pisi Farm Administration Console')
