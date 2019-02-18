16acc8c7382b7ef0d652e75df353c6ea1e7a956f
Safa Arıman <safa.ariman@sekomyazilim.com.tr>
2019-02-18 17:06:45 +0300

    updated allowed hosts

--- farm/settings.py
+++ farm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pisifarm.herokuapp.com', ]
 
 
 # Application definition
